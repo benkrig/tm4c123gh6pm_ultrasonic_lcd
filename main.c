cba189583a47cc8b86320a7d50e9d5c93d7ef041
Ben Krig <benjaminjkrig@gmail.com>
2019-04-29 12:15:53 -0700

    fix indentation with vim auto indentwq

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "LCD.h"
 
 int main(void) {
-    lcd_init();
-    while(1){
-        lcd_goto(1,1);
-        lcd_puts("LCD Testing");
-        lcd_goto(2,1);
-        lcd_puts("SJSU");
-			
-        SysCtlDelay(SysCtlClockGet()/2);
-        lcd_clear();
-		}
+	lcd_init();
+	while(1){
+		lcd_goto(1,1);
+		lcd_puts("LCD Testing");
+		lcd_goto(2,1);
+		lcd_puts("SJSU");
+
+		SysCtlDelay(SysCtlClockGet()/2);
+		lcd_clear();
+	}
 }
